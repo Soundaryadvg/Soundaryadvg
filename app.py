ab60bc6d7a2c0b98f6b5bc338393abdb76b44fef
Soundaryadvg <139335640+Soundaryadvg@users.noreply.github.com>
2023-07-24 16:02:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,74 +1,30 @@
-# Core Pkgs
-import streamlit as st
-
-# EDA Pkgs
-import pandas as pd
+from flask import Flask;
+from flask import render_template,request
 import numpy as np
-
-# Utils
-import os
 import joblib
 
-# Data Viz pkgs
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
 
+app=Flask(__name__)
 
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    data=[]
 
-def main():
-    """Mortality Prediction App"""
-    st.title("Disease Mortality Prediction App")
+    for item in request.form.values():
+        data.append(float(item))
 
-    menu = ["Home","Login","SignUp"]
-    submenu =["Plot","Pridiction","Metrics"]
+    testdata=np.array([data])
 
-    choice = st.sidebar.selectbox("Menu",menu)
-    if choice == "Home":
-        st.subheader("Home")
-        st.text("What is Hepatitis?")
+    print(testdata)
+    model=joblib.load('fetalmodel.pkl')
+    res=model.predict(testdata)
 
-    elif choice == "Login":
-        username = st.sidebar.text_input("Username")
-        password = st.sidebar.text_input("Password",type='password')
-        if st.sidebar.checkbox("Login"):
-            if password == "1234":
-                st.success("Welcome {}".format(username))
-                st.success("Logged In as {}".format(username))
+    return render_template('index.html',p=res[0])
+@app.route('/')
+def index():
 
-                activity = st.selectbox("Activity",submenu)
-                if activity == "Plot":
-                    st.subheader("Data Vis Plot")
+    return render_template('index.html')
 
-                elif activity == "Prediction":
-                    st.subheader("Prediction Analytics")
+if __name__ =='__main__':
 
-                   
-
-            else:
-                    st.warning("Incorrect Username/Password")
-      
-    elif choice == "SignUp":
-        new_username = st.text_input("User name")
-        new_password = st.text_input("Password",type='password')
-
-        confirm_password = st.text_input("Confirm Password",type='password')
-        if new_password == confirm_password:
-            st.success("Password Confirmed")
-
-        else:
-            st.warning("Password not same")
-
-        if st.button("Submit"):
-           
-            st.success("You have sucessfully create a new account")
-            st.info("Login to Get Started")
-
-
-
-if __name__== '__main__':
-    main()    
-
-            
-
-
+    app.run(debug=True,host="0.0.0.0")
